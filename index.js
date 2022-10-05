167447006716b90b6c9861292bc0c508a260063b
SalmanDoria <salmandoria@gmail.com>
2022-10-05 14:23:09 -0400

    commit tree 2b95a8a6843395fe65d187e7a0ac7b52fbcfc365

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution in this file!
+const employee = {
+    Bob : "72nd",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){ // what exacly does this do?
+const updatedEmployee = {...employee};
+updatedEmployee[key] = value;
+return updatedEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(name, streetaddress, Broadway){ //what happended to broadway  // point of (obj,key)
+name[streetaddress] =  "12 Broadway"
+return name
+
+}
+
+function deleteFromEmployeeByKey(object, key) {
+const copyObject =  {...object};
+delete copyObject[key]                        //how to do with key    , what does only newEmployeeList = employee do
+return copyObject
+}
+
+const myObject = {
+    name : "Sam",       
+    favoriteFood : 'Pizza'
+
+}
+
+//console.log(deleteFromEmployeeByKey(myObject, "favoriteFood"))
+//console.log(myObject)
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+delete employee[key]
+return employee
+}
+
+
+
+
+/*function deleteFromEmployeeByKey(employee, key) {
+delete employee.Bob       //how to do with key
+return employee
+}*/
+/*
+const updatedEmployee = {...employee}
+updatedEmployee[Bob] = "73rd"
+return updatedEmployee;
+*/
